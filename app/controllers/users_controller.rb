57b9245bf2126be208eae5e8d55168582074c630
gadrielag <adorados@unal.edu.co>
2018-12-04 23:20:23 -0500

    password changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     # Method to create a new user using the safe params we setup.
     def create
         @user = User.new(user_params)
-        @user.password = params[:password]
+        # @user.password = params[:password]
         # if @user.save
         # render json: {status: 200, msg: 'User was created.'}
         # end
