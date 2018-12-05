cb060fb2bfab92a52c0cdcbd9225a498b343f835
gadrielag <adorados@unal.edu.co>
2018-12-04 23:48:06 -0500

    authen fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-    protect_from_forgery with: :null_session
+    skip_before_action :verify_authenticity_token
     # Include Knock within your application.
     include Knock::Authenticable
     
