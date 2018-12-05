c311a75a04639cccc9983da36dfe9479d42724c0
gadrielag <adorados@unal.edu.co>
2018-12-04 23:12:44 -0500

    fix create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         @user.password = params[:password]
-        if user.save
+        if @user.save
         render json: {status: 200, msg: 'User was created.'}
         end
     end
