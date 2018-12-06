976a8139abf23ad52cd99410c690abee43890de8
gadrielag <adorados@unal.edu.co>
2018-12-05 22:12:08 -0500

    change ldap direction

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # post 'user_token' => 'user_token#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Home controller routes.
+  resources :ldap
   root   'home#index'
   get    'auth'            => 'home#auth'
     
